93cbd185db523cfe72de543f42cb7cb6ca71e84e
JasonMorris1 <jasonbm123@gmail.com>
2023-02-09 23:07:53 -0800

    Added test dependencies and added example roboelectric test

---
+++ app/src/test/java/edu/ucsd/cse110/cse_110_project_cse_110_team_9/MainActivityTest.java
@@ -0,0 +1,15 @@
+package edu.ucsd.cse110.cse_110_project_cse_110_team_9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    @Test
+    public void test1(){
+        assertEquals(10,10);
+    }
+
+}
