6780550e3c2ad4b5c7f4662aac7cca18fe1a3c5d
JasonMorris1 <jasonbm123@gmail.com>
2023-02-10 12:30:00 -0800

    Added google maps api key, and maps default activity

--- app/src/main/java/edu/ucsd/cse110/cse_110_project_cse_110_team_9/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse_110_project_cse_110_team_9/MainActivity.java
@@ -2,7 +2,9 @@ package edu.ucsd.cse110.cse_110_project_cse_110_team_9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnLaunchMap(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
