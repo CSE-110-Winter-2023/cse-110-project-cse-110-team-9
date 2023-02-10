d98e65d3ccb96845b448aaf5f793dc7779beca78
JasonMorris1 <jasonbm123@gmail.com>
2023-02-09 22:54:12 -0800

    added empty actvity

---
+++ app/src/main/java/edu/ucsd/cse110/cse_110_project_cse_110_team_9/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.ucsd.cse110.cse_110_project_cse_110_team_9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/edu/ucsd/cse110/cse_110_project_cse_110_team_9/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.ucsd.cse110.cse_110_project_cse_110_team_9;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
