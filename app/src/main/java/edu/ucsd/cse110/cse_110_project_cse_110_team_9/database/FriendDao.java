013d394f081b5f981f9138124983a7e828bedcd4
JasonMorris1 <jasonbm123@gmail.com>
2023-03-05 19:42:52 -0800

    added friend database clasS

--- app/src/main/java/edu/ucsd/cse110/cse_110_project_cse_110_team_9/database/FriendDao.java
+++ app/src/main/java/edu/ucsd/cse110/cse_110_project_cse_110_team_9/database/FriendDao.java
@@ -2,9 +2,12 @@ package edu.ucsd.cse110.cse_110_project_cse_110_team_9.database;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Query;
 import androidx.room.Update;
 import androidx.room.Upsert;
+
+import java.util.List;
 
 @Dao
 public abstract class FriendDao {
@@ -13,8 +16,15 @@ public abstract class FriendDao {
     @Upsert
     public abstract void upsertFriend(Friend friend);
 
-    @Query("SELECT * FROM friends WHERE public_uid = :public_uid")
-    public abstract LiveData<Friend> get(String public_uid);
+
+    //Not really needed for our project
+//    @Query("SELECT * FROM friends WHERE public_uid = :public_uid")
+//    public abstract LiveData<Friend> get(String public_uid);
+
+    //Get all the friends in the local data base
+    @Query("SELECT * FROM friends ORDER BY public_uid")
+    public abstract LiveData<List<Friend>> getAll();
+
 
 
 }
